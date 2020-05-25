0e43b78e91cfa07d51793b4cd7f82782ee91378c
solomon103098 <solomon103098@gmail.com>
2020-05-26 02:28:03 +0800

    fixed

--- app/main/model/user.py
+++ app/main/model/user.py
@@ -30,27 +30,27 @@ class User(db.Model):
     def __repr__(self):
         return "<User '{}'>".format(self.username)
 
-def encode_auth_token(self, user_id):
-        """
-        Generates the Auth Token
-        :return: string
-        """
-        try:
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': user_id
-            }
-            return jwt.encode(
-                payload,
-                key,
-                algorithm='HS256'
-            )
-        except Exception as e:
-            return e
+    def encode_auth_token(self, user_id):
+            """
+            Generates the Auth Token
+            :return: string
+            """
+            try:
+                payload = {
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+                    'iat': datetime.datetime.utcnow(),
+                    'sub': user_id
+                }
+                return jwt.encode(
+                    payload,
+                    key,
+                    algorithm='HS256'
+                )
+            except Exception as e:
+                return e
 
-@staticmethod  
-  def decode_auth_token(auth_token):
+    @staticmethod  
+    def decode_auth_token(auth_token):
         """
         Decodes the auth token
         :param auth_token:
